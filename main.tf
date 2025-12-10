cc532b84b08e82e4b1dff340ace4fd7c43485423
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-12-10 20:16:29 +0100

    added subnet

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_subnet" "subnet2" {
+  name                 = var.subnet2_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet2_prefix]
+}

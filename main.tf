d6c999dd45b53faf92cae001ec446e13ed8ed530
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-12-10 19:50:46 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "MyResourceGroup"
+location            = "East US"
+vnet_name           = "MyVNet"
+subnet_name         = "MySubnet"
